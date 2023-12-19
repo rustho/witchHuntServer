1b403c5bb6f66b896bbcccd422c2b6f474d6eab3
Леонид Иванов <ivanov.l@robotkarl.com>
2023-12-19 15:58:14 +0800

    fix

--- src/database.module.ts
+++ src/database.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://leo:hXghunyFKd3A0Iaq@cluster0.luawq.mongodb.net/'),
+    MongooseModule.forRoot(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/`),
   ],
 })
 export class DatabaseModule {}
